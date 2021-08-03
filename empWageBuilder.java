f81c95d162f7c0806483ac81b8f7b3f07152bc96
ashwith31 <ashwitash@gmail.com>
2021-08-03 20:40:14 +0530

    modified:Calculated wages for a month

--- empWageBuilder.java
+++ empWageBuilder.java
@@ -9,31 +9,33 @@ public class empWageBuilder {
 		final int WAGE_PER_HR=20;
 		final int FULL_DAY_HR=8;
 		final int PART_TIME_HR=4;
+		final int WORKING_DAYS=20;
 		int salary=0;
+		int time=0;
 		int empCheck =(int) Math.floor(Math.random() * 10) % 3;
 		
 		switch(empCheck) 
 		
 		{
 			case IS_PRESENT_FULL_TIME :
-			salary=FULL_DAY_HR*WAGE_PER_HR;
 			System.out.println("The Employee is present Full Time");
-			System.out.println("Salary= "+salary);
+			time=FULL_DAY_HR;
 			break;
 		
 			case IS_PRESENT_HALF_TIME :
-			salary=PART_TIME_HR*WAGE_PER_HR;
 			System.out.println("The Employee is present Part Time");
-			System.out.println("Salary= "+salary);
+			time=PART_TIME_HR;
 			break;
 		
 			default:
-		
-			System.out.println("Employee is absent");;
-			System.out.println("Salary = "+salary);
 			break;
 			
 		}
+		for(int i=1;i<=WORKING_DAYS;i++)
+		{
+			salary=time*WAGE_PER_HR+salary;
+		}
+		System.out.println("Salary for a month = "+salary);
+	}
 
-}
 }
