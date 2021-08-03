b3ba9df10910176f65172a042f5ac189118496ec
ashwith31 <ashwitash@gmail.com>
2021-08-03 20:21:22 +0530

    modified:Solved using switch case statements

--- empWageBuilder.java
+++ empWageBuilder.java
@@ -4,31 +4,36 @@ public class empWageBuilder {
 
 	public static void main(String[] args) {
 
-		int IS_PRESENT_FULL_TIME =1;
-		int IS_PRESENT_HALF_TIME =2;
-		int WAGE_PER_HR=20;
-		int FULL_DAY_HR=8;
-		int PART_TIME_HR=4;
+		final int IS_PRESENT_FULL_TIME = 1;
+		final int IS_PRESENT_HALF_TIME = 2;
+		final int WAGE_PER_HR=20;
+		final int FULL_DAY_HR=8;
+		final int PART_TIME_HR=4;
 		int salary=0;
 		int empCheck =(int) Math.floor(Math.random() * 10) % 3;
-		if (empCheck == IS_PRESENT_FULL_TIME)
+		
+		switch(empCheck) 
+		
 		{
+			case IS_PRESENT_FULL_TIME :
 			salary=FULL_DAY_HR*WAGE_PER_HR;
 			System.out.println("The Employee is present Full Time");
 			System.out.println("Salary= "+salary);
-		}
-		else if (empCheck == IS_PRESENT_HALF_TIME)
-		{
+			break;
+		
+			case IS_PRESENT_HALF_TIME :
 			salary=PART_TIME_HR*WAGE_PER_HR;
 			System.out.println("The Employee is present Part Time");
 			System.out.println("Salary= "+salary);
-		}
-		else
-		{
+			break;
+		
+			default:
+		
 			System.out.println("Employee is absent");;
 			System.out.println("Salary = "+salary);
+			break;
+			
 		}
-
 
 }
 }
