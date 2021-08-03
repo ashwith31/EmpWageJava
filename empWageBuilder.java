c4b47fea578e6e806807cbd305be513b6c569cf2
ashwith31 <ashwitash@gmail.com>
2021-08-03 19:12:10 +0530

    Welcome message

--- empWageBuilder.java
+++ empWageBuilder.java
@@ -4,17 +4,8 @@ public class empWageBuilder {
 
 	public static void main(String[] args) {
 
-		int IS_PRESENT =1;
-		int empCheck =(int) Math.floor(Math.random() * 10) % 2;
-		if (empCheck == IS_PRESENT)
-		{
-			System.out.println("Present");
+	System.out.println("Welcome to Employee Wage Problem");
+
 		}
-		else
-		{
-			System.out.println("Absent");
-		}
-		
-	}
 
 }
